f014ab34cab80f7f2e2309f974d8654d2ec68746
melonbox\melon <melon@meloncolle.com>
2024-08-24 23:49:28 -0700

    Add power meter graphics

--- UI/powerMeter.gd
+++ UI/powerMeter.gd
@@ -1,6 +1,21 @@
-extends Polygon2D
+extends Sprite2D
 
-@onready var bar = $Bar
+@onready var bar = $Mask/Bar
 
+var power: float = 0
+
+var max_angle: float = PI * 2.12
+var min_angle: float = PI / 2.6
+
+func _ready():
+	bar.connect("draw", draw_bar)
+	
 func _on_changed_power(newPower: float):
-	bar.scale.y = newPower
+	power = newPower
+	draw_bar()
+	
+func draw_bar():
+	var lerpval = lerp(min_angle, max_angle, power)
+	var thickness = lerp(50, 200, power) # line gets glitchy if it's too thick on low fuel
+	bar.draw_arc(Vector2(-22, 10), 150, min_angle, lerpval, 32, Color.WHITE, thickness, true)
+	bar.queue_redraw()
